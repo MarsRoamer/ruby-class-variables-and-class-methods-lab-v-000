3325661c9521180d3610a7222ec1fde4a84f3e62
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-05 18:42:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ def initialize(name, artist, genre)
   @artist = artist
   @genre = genre
   @@count += 1
-  if @@artists.include?(artist)
+  if !@@artists.include?(artist)
     @@artists << artist
   end
 
